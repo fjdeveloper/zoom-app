967bbfaa28c946f55d891ebcf18d8797dc68d1b3
fjdeveloper <fjdeveloper@gmail.com>
2023-02-14 12:59:47 +0000

    ver por que cambia el parametro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 
-	export let c = $page.url.searchParams.get('c');
-	let email: string = '';
+	const c = $page.url.searchParams.get('c');
+	let e: string = '';
 </script>
 
 <div class="flex justify-center items-center h-screen">
 	<form
 		method="GET"
-		action="https://l5amgfqtc7.execute-api.us-east-2.amazonaws.com/production/{c}?e={email}"
+		action="https://l5amgfqtc7.execute-api.us-east-2.amazonaws.com/production/{c}?e={e}"
 	>
 		<div class="flex flex-col items-center space-y-3">
 			<div>
@@ -18,7 +18,7 @@
 					name="email"
 					class="rounded-xl"
 					placeholder="type your email"
-					bind:value={email}
+					bind:value={e}
 				/>
 			</div>
 			<button type="submit" class="bg-blue-500 text-white p-2 px-3 rounded-xl hover:bg-blue-600"
