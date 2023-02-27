6114f23b9ddf17e7986ed569ecf46ecc409abb97
fjdeveloper <fjdeveloper@gmail.com>
2023-02-27 12:53:24 +0000

    otro

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 
 <style lang="postcss">
 	:global(body) {
-		background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('back.png');
+		background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/back.png');
 		background-position: center center;
 		background-repeat: no-repeat;
 		background-size: cover;
